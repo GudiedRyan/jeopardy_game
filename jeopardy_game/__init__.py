fe89d093128ccec0f8661d67744028f0d9348787
Ryan Guide <ryan.guide@comcast.net>
2021-04-12 16:08:41 -0400

    Created user blueprint

--- jeopardy_game/__init__.py
+++ jeopardy_game/__init__.py
@@ -6,6 +6,8 @@ def create_app(config_class=Config):
     app.config.from_object(Config)
 
     from jeopardy_game.main.routes import main
+    from jeopardy_game.users.routes import users
     app.register_blueprint(main)
+    app.register_blueprint(users)
 
     return app
