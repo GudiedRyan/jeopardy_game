c1738764cc39e8900d49abb67848e930f8e8b5bf
Ryan Guide <ryan.guide@comcast.net>
2021-04-14 15:43:57 -0400

    Forms management

--- jeopardy_game/main/forms.py
+++ jeopardy_game/main/forms.py
@@ -1,15 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length
 
-class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=30)])
-    # Password
-    # Confirm Password
-    # Email?
-    submit = SubmitField('Register')
-
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Log In')
+class JeopardyForm(FlaskForm):
+    # Need 5 topics, and then point values from 100 - 400?
+    submit = SubmitField('Create')
