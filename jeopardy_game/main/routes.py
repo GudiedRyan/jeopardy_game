2457ac67932a764665fd2ff6d4efee95ef07bc34
Ryan Guide <ryan.guide@comcast.net>
2021-04-22 15:50:54 -0400

    Quick fix to form

--- jeopardy_game/main/routes.py
+++ jeopardy_game/main/routes.py
@@ -3,7 +3,7 @@ from flask import render_template, Blueprint
 main = Blueprint('main', __name__)
 
 @main.route('/')
-@main.route('/home')
+@main.route('/home', methods=['GET'])
 def home():
     return render_template('home.html', title="Home")
 
